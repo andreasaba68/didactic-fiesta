d0efb65bf17efb7871da12f49c07c35e15bcc06b
Andrea <Andrea@DESKTOP-9DVLCQ2>
2019-05-09 16:16:54 +0200

    added test for session stickness

--- docker-swarm-1/src/docker/007 build.sh
+++ docker-swarm-1/src/docker/007 build.sh
@@ -10,22 +10,29 @@ mvn package -Dmaven.test.skip=true
 docker build . --tag=alpinejavaswarm --file './src/docker/015 docker-swarm.Dockerfile'
 
 # *** Creating the services, ~/data is needed for visualizer service
-docker swarm init
+#docker swarm init
 
 docker stack deploy -c './src/docker/070 docker-compose.yml' dockerswarm1
 
+#correct way:
+docker node ls
+#get the manager (*red) id then:
+docker node inspect --format '{{.Status.Addr}}' <the id e.g. t4leolv16u2jqw7kdsfvk3x7h>
+#take the ip address and then you can
+curl -4 http://the.ip.addr.ess:8081/session/write?test=pioppino
+curl -4 http://the.ip.addr.ess:8081/session/write?test=pioppino
+curl -4 http://ip-172-31-11-127.sa-east-1.compute.internal:8081/session/write?test=pioppino
+
+#take the DefaultDNSTarget from CloudFormation's "Outputs" tab and use it similarly e.g.
+curl -4 http://Docker-ExternalLoa-8L1FZ6572MMM-2059149846.sa-east-1.elb.amazonaws.com/session/write?test=pioppino
+
+
+#commands used to debug, the problem was elsewhere though
 #docker run -p 8081:8081 alpinejavaswarm
-#docker swarm join --token SWMTKN-1-1j3x29v9ollequal9t75xbo8wo40q8d7mnwle274ezfj6fb8ah-azvnw0tvhaturqlxulyltpfy7 192.168.43.27:2377
-docker container ls
-docker exec -ti dockerswarm1_web.1.g1a4m6qpyotlkx7utq5k2361u /bin/ash
+#docker container ls
+#docker exec -ti dockerswarm1_web.1.fqan3i5a5hy1l1qp1sa53devy /bin/ash
+#netstat -na | grep -i LISTEN
+#curl http://127.0.0.1:8081/session/write?test=pioppino
 
-#sudo apk add docker-compose
-#source https://wiki.alpinelinux.org/wiki/Docker#Docker_Compose
-#apk add py-pip
-#apk add python-dev libffi-dev openssl-dev gcc libc-dev make
-#pip install docker-compose
-#docker-compose up -d -f './src/docker/070 docker-compose.yml'
-
-
-docker service update --publish-add published=8080,target=8080 dockerswarm1_visualizer
-docker service update --publish-add published=8081,target=8081 dockerswarm1_web
+#docker service update --publish-add published=8080,target=8080 dockerswarm1_visualizer
+#docker service update --publish-add published=8081,target=8081 dockerswarm1_web
--- docker-swarm-1/src/test/java/it/asaba/sdev/dockerswarm1/DockerSwarm1ApplicationTests.java
+++ docker-swarm-1/src/test/java/it/asaba/sdev/dockerswarm1/DockerSwarm1ApplicationTests.java
@@ -1,18 +1,12 @@
 package it.asaba.sdev.dockerswarm1;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.htmlunit.webdriver.MockMvcHtmlUnitDriverBuilder;
@@ -20,53 +14,19 @@ import org.springframework.test.web.servlet.htmlunit.webdriver.MockMvcHtmlUnitDr
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
-public class DockerSwarm1ApplicationTests {
+public class DockerSwarm1ApplicationTests extends DockerSwarm1ApplicationTests2 {
 
-  private String baseUrl = "http://127.0.0.1:8080";
-
-  @Autowired
-  private MockMvc mockMvc;
-
-  @LocalServerPort
-  protected int serverPort;
-
-  private WebDriver driverS1;
-  private WebDriver driverS2;
+  @Autowired private MockMvc mockMvc;
 
   @Before
   public void setup() {
-    System.out.println("DockerSwarm1ApplicationTests.setup() "+serverPort);
     this.driverS1 = MockMvcHtmlUnitDriverBuilder.mockMvcSetup(this.mockMvc).build();
     this.driverS2 = MockMvcHtmlUnitDriverBuilder.mockMvcSetup(this.mockMvc).build();
   }
 
-  @Test
+  @Test @Override
   public void concurrentSessionTest() {
-    String testString1 = System.currentTimeMillis()+"@test1";
-    String testString2 = System.currentTimeMillis()+"@test2";
-
-    // writes s1 to session 1
-    test0(driverS1,testString1);
-
-    // writes s2 to session 2
-    test0(driverS2,testString2);
-
-    // expects session 1 to still have s1
-    driverS1.get(baseUrl + "/session/read");
-    assertEquals(testString1, driverS1.findElement(By.cssSelector("h1")).getText());
-
-    // session must be different anyway (checking the memory address)
-    assertNotEquals(driverS1.findElement(By.cssSelector("h3")).getText(),driverS2.findElement(By.cssSelector("h3")).getText());
-
-    // ipaddress or server name
-    assertNotEquals(driverS1.findElement(By.cssSelector("h2")).getText(),driverS2.findElement(By.cssSelector("h2")).getText());
-
+    super.concurrentSessionTest();
   }
-
-  private void test0(WebDriver driver, String testString) {
-    driver.get(baseUrl + "/session/write?test="+testString);
-    assertEquals(testString, driver.findElement(By.cssSelector("h1")).getText());
-  }
-
 
 }
